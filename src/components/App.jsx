c6120e733dceb6b5c0cc86254d28ff32b2b66c4e
Steven McConomy <srmcconomy@gmail.com>
2016-12-10 18:04:27 -0800

    asdf

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,44 @@
 // @flow
 
-import React from 'react';
+import React, { Component } from 'react';
 
-export default function () {
-  return <div>HI</div>;
+import Overlay from './Overlay';
+import store from '../util/store';
+import StreamArea from './StreamArea';
+import Toolbar from './Toolbar';
+
+type State = {
+  aspect: string,
+}
+
+export default class App extends Component {
+  state: State;
+
+  constructor() {
+    super();
+    const data = store.get();
+    this.state = { aspect: data.aspect };
+  }
+
+  _onStoreChange = () => {
+    const data = store.get();
+    this.setState({ aspect: data.aspect });
+  }
+
+  componentDidMount() {
+    store.addChangeListener(this._onStoreChange);
+  }
+
+  componentWillUnmount() {
+    store.removeChangeListener(this._onStoreChange);
+  }
+
+  render() {
+    return (
+      <div className={'App ' + this.state.aspect}>
+        <Toolbar />
+        <StreamArea />
+      </div>
+    );
+  }
 }
--- src/server.jsx
+++ src/server.jsx
@@ -1,19 +1,35 @@
 // @flow
 
+import { List } from 'immutable';
 import express from 'express';
+import fs from 'fs';
+import http from 'http';
 import path from 'path';
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
+import socketio from 'socket.io'
 
-import config from '../config';
+import './util/SRL';
+import { TransformRecord, DataRecord, StreamRecord } from './util/Records';
 import App from './components/App';
+import config from '../config';
+import dispatcher from './util/dispatcher';
+import store from './util/store';
 
-global.navigator = { navigator: 'all' };
+import type { Payload } from './util/dispatcher';
+import type EventEmitter from 'events';
 
 const app = express();
-app.use('/assets/', express.static(
-  path.join(__dirname, '../build/static')
+const server = http.Server(app);
+const io: EventEmitter = socketio(server);
+
+server.listen(process.env.PORT || config.ports.express);
+
+app.use('/assets', express.static(
+  path.join(__dirname, 'static')
 ));
+
+app.use('/images', express.static(path.join(__dirname, '../images')))
 
 let jsFile;
 if (process.env.NODE_ENV === 'production') {
@@ -22,21 +38,64 @@ if (process.env.NODE_ENV === 'production') {
   jsFile = `http://localhost:${config.ports.webpack}/${config.files.client.out}/${config.files.client.outFile}`;
 }
 
-app.use((req, res) => {
-  const content = ReactDOMServer.renderToString(<App />);
-  res.send(
-    `<html>
-      <head>
-        <link rel="stylesheet" href="/assets/css/main.css" />
-      </head>
-      <body>
-        <div id="react-root">${content}</div>
-        <script async defer src="${jsFile}"></script>
-      </body>
-    </html>`
-  );
+app.get('/', (req, res) => {
+  const build = ReactDOMServer.renderToString(<App />);
+  const html = `<!doctype html>
+  <html lang="en">
+    <head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="shortcut icon" href="./src/favicon.ico">
+      <link rel="stylesheet" type="text/css" href="/assets/css/main.css">
+      <title>Commentary Helper</title>
+    </head>
+    <body>
+      <div id="root">${build}</div>
+      <script async defer src="${jsFile}"></script>
+    </body>
+  </html>`
+  res.send(html);
 });
 
-app.listen(config.ports.express, () => {
-  console.log(`Listening on port ${config.ports.express}`);
+app.get('/save', (req, res) => {
+  const data = store.get();
+  fs.writeFile('./data.json', JSON.stringify(data.toJS()), function(err) {
+    if(err) {
+        return console.log(err);
+    }
+    res.send("SAVED!");
+  });
+});
+
+app.get('/load', (req, res) => {
+  const data = store.get();
+  fs.readFile('./data.json', 'utf8', (err, data) => {
+    if (err) throw err;
+    store.set(DataRecord.fromJS(JSON.parse(data)));
+    res.send("LOADED!");
+  });
+});
+
+io.on('connection', function(socket: EventEmitter) {
+  socket.emit('set', store.get());
+  const token = dispatcher.register(payload => {
+    socket.emit('dispatch', payload);
+  });
+  socket.on('dispatch', (payload) => {
+    switch(payload.type) {
+      case 'set-transform':
+        payload.transform = new TransformRecord(payload.transform);
+        break;
+
+      case 'set-stream': case 'set-and-select-stream':
+        payload.stream = new StreamRecord(payload.stream);
+        break;
+
+      case 'set-race':
+        payload.entrants = List(payload.entrants);
+        break;
+    }
+    dispatcher.broadcast(payload, token);
+  });
+  socket.on('disconnect', () => dispatcher.unregister(token));
 });
